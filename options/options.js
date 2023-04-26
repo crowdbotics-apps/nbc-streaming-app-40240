8516f4373748565456bb8df787a9b5185cb2b39b
ZackCB <zachary@crowdbotics.com>
2023-04-26 17:55:48 -0600

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "nbc-universal-studi-40240",
-  url: "https://nbc-universal-studi-40240.botics.co",
-  api: "https://nbc-universal-studi-40240.botics.co/api/v1"
-}
+  "name": "nbc-universal-studi-40240",
+  "url": "https://nbc-universal-studi-40240.botics.co",
+  "api": "https://nbc-universal-studi-40240.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
