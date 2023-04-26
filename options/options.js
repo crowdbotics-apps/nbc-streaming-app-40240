0058e2b94397d033a100e0c205577d4aec69386f
Crowdbotics <team@crowdbotics.com>
2023-04-26 19:40:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "nbc-universal-studi-40240",
+  url: "https://nbc-universal-studi-40240.botics.co",
+  api: "https://nbc-universal-studi-40240.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
